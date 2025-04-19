5d9a24028f82fd5275b817bd4b4a8d2950f1aff6
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2025-04-19 15:54:12 -0500

    Ejercicio 1 practica basica

---
+++ 1_fundamentos/practica.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string nombre;
+    int edad;
+
+    cout << "Ingresa tu nombre: ";
+    cin >> nombre;
+
+    cout << "Ingresa tu edad: ";
+    cin >> edad;
+
+    cout << "Hola, mi nombre es " << nombre << " y mi edad es " << edad << " años" << endl;
+}
