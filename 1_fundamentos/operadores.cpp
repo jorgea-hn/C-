58b097129a3e589640fc9ba8c19676c57a317e22
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2025-04-19 15:43:47 -0500

    Operadores y tipos de operadores

---
+++ 1_fundamentos/operadores.cpp
@@ -0,0 +1,37 @@
+// operadores aritmeticos
+
+// + suma
+// - resta
+// * multiplicacion
+// / division
+// % modulo
+
+
+// operaciones relacionales
+// == igual
+// != diferente
+// < menor que
+// > mayor que
+// >= mayor o igual
+// <= menor o igual
+
+/* operadores logicos
+&& and
+|| or
+! not
+*/
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int a = 10, b=5;
+    cout << a + b << endl;
+    cout << (a>b) << endl;
+    cout << (a>5 && b<10) << endl;
+}
