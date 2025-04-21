98b42232cf24281a1c35050b8de60030a8f7f17f
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2025-04-21 10:02:46 -0500

    Condicional if else

---
+++ 2_Control_flujo_y_funciones/condicional_if_else.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int edad = 20;
+
+    if (edad >= 18){
+        cout << "Es mayor de edad" << endl;
+    }
+    else{
+        cout << "Es menor de edad" << endl;
+    }
+}
